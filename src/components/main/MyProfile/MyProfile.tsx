ce34b0c375ee128f573c9042b468c17dda13dcff
svetlana nemova <photiniya@gmail.com>
2023-02-03 14:18:50 +0300

    Send My Post block styling done. Сhanged color palette.

--- src/components/main/MyProfile/MyProfile.tsx
+++ src/components/main/MyProfile/MyProfile.tsx
@@ -4,14 +4,14 @@ import './MyProfile.css';
 export function MyProfile () {
     return (
     <div className={'profile'}>
-        <img className={'avatar'} alt={'avatar'} src="https://cs12.pikabu.ru/post_img/big/2022/04/16/4/1650082940124498859.jpg"/>
-        <div className={'data'}>
-            <h3 className={'title'}>Svetlana N.</h3>
-            <div className={'descr'}>
-                <div>Date of Birth: 31 january</div>
-                <div>City: Rybinsk</div>
-                <div>Education: YGMU'13</div>
-                <div>Web-site: none</div>
+        <img className={'profile__avatar'} alt={'avatar'} src="https://cs12.pikabu.ru/post_img/big/2022/04/16/4/1650082940124498859.jpg"/>
+        <div className={'profile__data'}>
+            <h3 className={'profile__title'}>Svetlana N.</h3>
+            <div className={'profile__descr'}>
+                <div><span>Date of Birth:</span> 31 january</div>
+                <div><span>City:</span> Rybinsk</div>
+                <div><span>Education:</span> YGMU'13</div>
+                <div><span>Web-site:</span> none</div>
             </div>
         </div>
     </div>
--- src/components/main/SendMyPost/SendMyPost.tsx
+++ src/components/main/SendMyPost/SendMyPost.tsx
@@ -3,10 +3,14 @@ import './SendMyPost.css';
 
 export function SendMyPost () {
     return(
-        <div>
+        <div className={'myPost'}>
             <h3>My posts</h3>
-            <input/>
-            <button>Send</button>
+            <div>
+                <pre>
+                    <textarea placeholder={'Write your post...'}></textarea>
+                </pre>
+                <button>Send</button>
+            </div>
         </div>
     )
 }
